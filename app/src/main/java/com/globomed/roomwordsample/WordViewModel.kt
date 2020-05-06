17bc603e17570b6f72fc94e7b3a12cf658aea659
Dennis Kimani <denoh6804@gmail.com>
2020-05-06 15:30:23 +0300

    ViewModel added

---
+++ app/src/main/java/com/globomed/roomwordsample/WordViewModel.kt
@@ -0,0 +1,28 @@
+package com.globomed.roomwordsample
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
+
+class WordViewModel(application: Application):AndroidViewModel(application){
+
+    //Hold reference of the Repository
+    private val repository:WordRepository
+
+    val allWords:LiveData<List<Word>>
+
+    init {
+        //Get the Database
+        val wordDao = WordRoomDatabase.getDatabase(application).wordDao()
+        //Pass the Database to the Repository which is a required parameter
+        repository= WordRepository(wordDao)
+        allWords= repository.allWords
+    }
+
+    fun insertWord(word: Word) = viewModelScope.launch {
+        repository.insert(word)
+    }
+
+}
