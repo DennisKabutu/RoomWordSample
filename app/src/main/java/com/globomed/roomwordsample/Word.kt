04465b2f8a2d40e22fa7b05a121a082d87cbe9c5
Dennis Kimani <denoh6804@gmail.com>
2020-05-06 13:10:02 +0300

    Entity created, to be used with Room

---
+++ app/src/main/java/com/globomed/roomwordsample/Word.kt
@@ -0,0 +1,12 @@
+package com.globomed.roomwordsample
+
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+
+@Entity(tableName = "word_table")
+/* Each @Entity class represents a Sql lite table
+* Annotate your class declaration to indicate that its an entity */
+data class Word (@PrimaryKey(autoGenerate = true) val id:Int,
+                 @ColumnInfo(name = "word") val word:String)
